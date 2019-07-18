565c1ef9b7b2b318ad92fdd02745d472badcc989
Alfonso Millan <alfonso.millan@giantmachines.com>
2019-07-18 13:35:11 -0400

    feat: init sensor java test

--- src/main/java/Sensor.java
+++ src/main/java/Sensor.java
@@ -1,25 +1,92 @@
-public class Sensor {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
+public class Sensor {
   private int lower;
   private int higher;
   private int[] buffer;
 
-  Sensor(int lower, int higher, int[] buffer) {
+  private int length;
+  private int[] filtered;
+  private ArrayList<Integer> arrayList;
 
+  Sensor(int lower, int higher, int[] buffer) {
+     // construct an array list
+     arrayList = new ArrayList<Integer>();
+
+     // filter out elements that are out of bounds
+     for (int val : buffer) {
+       if (val >= lower && val <= higher) {
+         arrayList.add(val);
+       }
+     }
+
+    // Sort the arrayList
+    Collections.sort(arrayList);
+
+    this.length = arrayList.size();
+    this.filtered = new int[this.length];
+    this.lower = lower;
+    this.higher = higher;
+    this.buffer = buffer;
   }
 
   public double findMean() {
-    return 0;
+    double mean = 0;
+    double sum = 0;
+
+    for (int i : arrayList) {
+      sum += i;
+    }
+
+    mean = sum / length;
+    return mean;
   }
 
   public double findMedian() {
-    return 0;
+    if (length % 2 == 0) { // avg of center-most 2 if even-lengthed
+      int rightIdx = length / 2;
+      int leftIdx = rightIdx - 1;
+
+      int leftVal = arrayList.get(leftIdx);
+      int rightVal = arrayList.get(rightIdx);
+      double median = (leftVal + rightVal) / 2.0;
+      return median;
+    } else { // middle value if odd-lengthed
+      int midIdx = length / 2;
+      double median = arrayList.get(midIdx);
+      return median;
+    }
   }
 
+  // the number that occurs the most often
   public int findMode() {
-    return 0;
-  }
+    HashMap<Integer, Integer> modeMap = new HashMap<Integer, Integer>();
 
+    int occurance = 1;
+    for (int key : arrayList) {
+      if (modeMap.containsKey(key)) {
+        occurance++;
+      }
+      modeMap.put(key, occurance);
+    }
+
+    // get the maxValue in the HashMap
+    int maxValue = Collections.max(modeMap.values());
+
+    int maxKey = arrayList.get(0);
+    // iterate through the map to find the key associated w/ the maxValue
+    for (Entry<Integer, Integer> entry : modeMap.entrySet()) {
+      if (entry.getValue() == maxValue) {
+        maxKey = entry.getKey();
+      }
+    }
+
+    return maxKey;
+  }
 
   public int getLower() {
     return lower;
@@ -33,4 +100,11 @@ public class Sensor {
     return buffer;
   }
 
+  public int[] getOutput() {
+    return filtered;
+  }
+
+  public String toString() {
+    return arrayList.stream().map(Object::toString).collect(Collectors.joining(","));
+  }
 }
